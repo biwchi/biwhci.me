ed5a8886567ae5cc4cc53ca6b16741b69066ca52
hodame04 <hodame04@gmail.com>
2024-07-20 16:33:09 +0500

    fix some bugs in leaf layout correct animation

--- components/LeafArt.tsx
+++ components/LeafArt.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from "@/providers";
 import { p5i, P5I } from "p5i";
 import { useEffect, useRef } from "react";
-import LEAF_IMAGE from "@/public/leaf.png";
+import LEAF_IMAGE from "@/public/leaf.svg";
 
 type Rotation = {
   axis: "x" | "y" | "z";
@@ -16,6 +16,7 @@ type Leaf = {
   x: number;
   y: number;
   z: number;
+  prevXSpeed: number;
   ySpeed: number;
   xSpeedVariation: number;
   rotation: Rotation;
@@ -24,6 +25,7 @@ type Leaf = {
 type Wind = {
   magnitude: number;
   maxSpeed: number;
+  minSpeed: number;
   duration: number;
   start: number;
   speed: (t: number, y: number) => number;
@@ -31,12 +33,13 @@ type Wind = {
 
 const DARK_COLOR = "#282627";
 const LIGHT_COLOR = "#ede2d9";
-const LEAF_COUNT = 20;
+const LEAF_COUNT = 40;
 
 const default_wind = {
-  magnitude: 1.2,
+  magnitude: 8,
   maxSpeed: 12,
-  duration: 500,
+  minSpeed: 4,
+  duration: 100,
   start: 0,
   speed: () => 0,
 };
@@ -48,14 +51,18 @@ export default function LeafArt() {
     mount,
     unmount,
     createCanvas,
+    angleMode,
+    DEGREES,
     WEBGL,
     background,
-    fill,
     sin,
-    texture,
     noStroke,
     image,
     loadImage,
+    CENTER,
+    imageMode,
+    degrees,
+    frameRate,
     PI,
   } = p5iRef.current;
 
@@ -69,10 +76,14 @@ export default function LeafArt() {
   const leaves = useRef<Leaf[]>([]);
   const wind = useRef<Wind>(default_wind);
 
-  function resetLeaf(leaf: Leaf) {
-    leaf.x = Math.random() * w - w / 2;
-    leaf.y = -10;
-    leaf.z = Math.random() * 200;
+  function resetLeaf(leaf: Leaf, frameCount = 0) {
+    leaf.x = -w / 2 - Math.random() * w * 1.75;
+    leaf.y = -h / 2;
+    leaf.z = Math.random() * 100 + 50;
+
+    if (frameCount === 0) {
+      leaf.y = (Math.random() * h) / 2;
+    }
 
     leaf.ySpeed = Math.random() + 1.5;
     leaf.xSpeedVariation = Math.random() * 0.8 - 0.4;
@@ -94,20 +105,21 @@ export default function LeafArt() {
 
   function updateLeaf(leaf: Leaf, frameCount = 0) {
     const speed = wind.current.speed(frameCount - wind.current.start, leaf.y);
-    const xSpeed = speed + leaf.xSpeedVariation;
+    let xSpeed = speed + leaf.xSpeedVariation;
+
     leaf.x += xSpeed;
     leaf.y += leaf.ySpeed;
     leaf.rotation.value += leaf.rotation.speed;
 
-    if (leaf.x > w + 10 || leaf.y > h + 10) {
-      resetLeaf(leaf);
+    if (leaf.x > w / 2 + 10 || leaf.y > h / 2 + 10) {
+      resetLeaf(leaf, frameCount);
     }
   }
 
   function updateWind(frameCount = 0) {
     const { duration, start } = wind.current;
 
-    if (!(frameCount == 0 || frameCount > start + duration)) {
+    if (!(frameCount == 1 || frameCount > start + duration)) {
       return;
     }
 
@@ -116,9 +128,14 @@ export default function LeafArt() {
       wind.current.magnitude * 50 + (Math.random() * 20 - 10);
     wind.current.start = frameCount;
 
-    wind.current.speed = (t: number, y: number) => {
-      const a = ((wind.current.magnitude / 2) * (h - (2 * y) / 3)) / h;
-      return a * sin(((2 * PI) / wind.current.duration) * t + (3 * PI) / 2) + a;
+    wind.current.speed = (frameCount: number, y: number) => {
+      const a = ((wind.current.magnitude / 2) * (h - (2 * (y / 2)) / 3)) / h;
+
+      const asin = sin(
+        degrees(((2 * PI) / wind.current.duration) * frameCount + (3 * PI) / 2)
+      );
+
+      return a * asin + a;
     };
   }
 
@@ -128,6 +145,7 @@ export default function LeafArt() {
         x: 0,
         y: 0,
         z: 0,
+        prevXSpeed: 0,
         ySpeed: 0,
         xSpeedVariation: 0,
         rotation: {
@@ -145,8 +163,11 @@ export default function LeafArt() {
 
   function setup() {
     createCanvas(w, h, WEBGL);
-    background("#ccc");
+    background(color);
     noStroke();
+    imageMode(CENTER);
+    angleMode(DEGREES);
+    frameRate(75);
 
     createLeaves();
   }
@@ -157,25 +178,35 @@ export default function LeafArt() {
     rotateY,
     rotateZ,
     translate,
-    plane,
+    push,
+    pop,
   }: P5I) {
-    background("#ccc");
+    background(color);
     updateWind(frameCount);
-    console.log(leaves)
+
     leaves.current.forEach((leaf) => {
       updateLeaf(leaf, frameCount);
       const w = leaf.z / 5;
       const h = leaf.z / 5;
 
-      if (leaf.rotation.axis === "x") {
+      push();
+      translate(leaf.x, leaf.y, leaf.z);
+
+      if (leaf.rotation.axis !== "x") {
         rotateX(leaf.rotation.x);
+      }
+
+      if (leaf.rotation.axis === "x") {
+        rotateX(leaf.rotation.value);
       } else if (leaf.rotation.axis === "y") {
         rotateY(leaf.rotation.value);
       } else {
         rotateZ(leaf.rotation.value);
       }
 
-      image(leafImage.current, leaf.x, leaf.y, w, h);
+      image(leafImage.current, 0, 0, w, h);
+
+      pop();
     });
   }
 
