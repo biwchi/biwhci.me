7ca18d56bcf6e1d33e796e1b7e4d72e0147d8ab6
hodame04 <hodame04@gmail.com>
2024-07-23 16:19:15 +0500

    eblan

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,54 @@
 import "../public/css/globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import NavBar from "@/components/NavBar";
 import ThemeProvider from "@/components/ThemeProvider";
 import LeafArt from "@/components/LeafArt";
 
+import { AppProps } from "next/app";
+import { Metadata } from "next";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Biwchi",
-  description: "Biwchi's personal website",
-};
+function setTheme() {
+  const prefersDark =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const setting =
+    localStorage.getItem("theme") || (prefersDark ? "dark" : "light");
+
+  if (setting === "dark" || (prefersDark && setting !== "light")) {
+    document.documentElement.classList.toggle("dark", true);
+  }
+}
 
 const themeSetter = `
-(function () {
-  const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-  const setting = localStorage.getItem('theme') || (prefersDark ? 'dark' : 'light')
-
-  if (setting === 'dark' || (prefersDark && setting !== 'light')) {
-    document.documentElement.classList.toggle('dark', true)
-  }
-    })()
+  ${setTheme}()
 `;
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type Props = {
   children: React.ReactNode;
-}>) {
+} & AppProps<{ meta?: Metadata }>;
+
+export default function RootLayout(props: Props) {
+  const { children, pageProps } = props;
+  console.log(props, 'EBLAN')
+
+  const title = pageProps?.meta?.title as string ?? "Biwchi";
   return (
     <html lang="en">
+      <head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+
       <body className={inter.className}>
         <script dangerouslySetInnerHTML={{ __html: themeSetter }}></script>
 
         <ThemeProvider>
           <LeafArt />
 
-          <div className='relative z-10'>
+          <div className="relative z-10">
             <NavBar />
             <main className="flex px-5 max-w-screen-sm mx-auto">
               {children}
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import Icons from "unplugin-icons/webpack";
 import createMdx from "@next/mdx";
+import { parse } from "acorn";
+
+import rehypeToc from "@stefanprobst/rehype-extract-toc";
+import rehypeExportToc from "@stefanprobst/rehype-extract-toc/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -17,10 +21,42 @@ const nextConfig = {
   },
 };
 
+/** @type {import('unified').Plugin<[Options]>} */
+function createNextStaticProps(map) {
+  return function transformer(tree) {
+    tree.children.push({
+      type: "mdxjsEsm",
+      data: {
+        estree: parse(
+          `
+          export const getStaticProps = () => ({
+            props: ${map},
+          })
+          `,
+          {
+            sourceType: "module",
+          }
+        ),
+      },
+    });
+  };
+}
+
 const withMDX = createMdx({
   options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+    rehypePlugins: [
+      rehypeExportToc,
+      rehypeToc,
+      [
+        createNextStaticProps,
+        `
+        {
+          meta,
+          tableOfContents,
+        }
+        `,
+      ],
+    ],
   },
 });
 
